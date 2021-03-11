6cfbbe043bc517985e7b9b87d3f8851a72cc18be
Ryan <hey@enricosola.com>
2021-03-11 22:15:23 +0100

    Implemented the help command.

--- src/CommandRouter.js
+++ src/CommandRouter.js
@@ -3,43 +3,52 @@
 const GuildConfig = require('./models/GuildConfig');
 
 class CommandRouter {
-    static _client = null;
-    static _commands = new Map();
+    static #client = null;
+    static #commands = new Map();
 
-    static async _handleMessage(message){
+    static async #getGuildConfig(guildID){
+        let guildConfig = await GuildConfig.find(guildID);
+        if ( guildConfig === null ){
+            guildConfig = new GuildConfig(guildID);
+            await guildConfig.save();
+        }
+        return guildConfig;
+    }
+
+    static async #handleMessage(message){
         if ( !message.author.bot ){
-            const guildConfig = await GuildConfig.find(message.channel.guild.id);
+            const guildConfig = await CommandRouter.#getGuildConfig(message.channel.guild.id);
             if ( guildConfig !== null && message.content.indexOf(guildConfig.getPrefix()) === 0 ){
                 const start = guildConfig.getPrefix().length;
                 const end = message.content.indexOf(' ');
                 const commandName = end === -1 ? message.content.substr(start) : message.content.substr(start, end - 1);
-                const command = CommandRouter._commands.get(commandName);
+                const command = CommandRouter.#commands.get(commandName);
                 if ( typeof command !== 'undefined' ){
                     message.cleanedContent = message.content.substr(start + commandName.length + 1);
-                    const controller = new command.controller(CommandRouter._client, guildConfig, message);
+                    const controller = new command.controller(CommandRouter.#client, guildConfig, message);
                     await controller[command.method]();
                 }
             }
         }
     }
 
-    static _init(){
-        CommandRouter._client.on('message', async (message) => {
-            await CommandRouter._handleMessage(message);
+    static #init(){
+        CommandRouter.#client.on('message', async (message) => {
+            await CommandRouter.#handleMessage(message);
         });
     }
 
     static setClient(client){
-        CommandRouter._client = client;
-        CommandRouter._init();
+        CommandRouter.#client = client;
+        CommandRouter.#init();
     }
 
     static getClient(){
-        return CommandRouter._client;
+        return CommandRouter.#client;
     }
 
     static registerCommand(command, controller, method){
-        CommandRouter._commands.set(command, {
+        CommandRouter.#commands.set(command, {
             controller: controller,
             method: method
         });
--- src/providers/bot/ChatProvider.js
+++ src/providers/bot/ChatProvider.js
@@ -3,10 +3,19 @@
 const lala = require('@lala.js/core');
 const CommandRouter = require('../../CommandRouter');
 const ChatController = require('../../controllers/bot/ChatController');
+const HelpService = require('../../services/HelpService');
+const LocaleManager = require('../../support/LocaleManager');
 
 class ChatProvider extends lala.Provider {
     static async setup(){
+        const labels = await LocaleManager.getLabelTranslationsMulti([
+            'help.chat.say',
+            'help.chat.help'
+        ]);
         CommandRouter.registerCommand('say', ChatController, 'say');
+        HelpService.registerCommand('say', 'Chat', labels['help.chat.say']);
+        CommandRouter.registerCommand('help', ChatController, 'help');
+        HelpService.registerCommand('help', 'Chat', labels['help.chat.help']);
     }
 }
 
